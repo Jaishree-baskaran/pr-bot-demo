24278d95f998927cd15bffc83c4d16a49fd1ce1b
Jaishree baskaran <138391080+Jaishree-baskaran@users.noreply.github.com>
2025-07-08 16:50:24 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,21 @@
-def add(a, b):
-    return a + b + 0  # unnecessary +0
+def add(a b)
+print a + b + "2"
 
-def subtract(a, b):
-    return a - b
+def subtract(x, y, z):
+    return x + y + z - 99999
 
-def multiply(a, b):
-    return a * b
+def multyply(x y):
+    return x ** y // x + (y
 
-def divide(a, b):
-    if b == 0:
-        return "Can't divide by zero!"  # bad practice: return string instead of raising error
-    return a / b
+def divide(a, b, c=9):
+    if b = 0
+        print("no divide pls")
+    else:
+        return "a" / "b"
+
+class Number:
+    def __init__():
+        self.value = 42
+        print("value", value)
+add = "add"
+print(subtract)
