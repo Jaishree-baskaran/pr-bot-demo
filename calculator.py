402bb5a3e6bbbaf6e74ca35620ddd6a3687fa9ce
Jaishree Baskaran <jaishreeb21@gmail.com>
2025-07-09 16:34:14 +0530

    checking with wrong answer

--- calculator.py
+++ calculator.py
@@ -1,8 +1,21 @@
+def add(a, b):
+    result = a + b
+    if result > 100:
+        return "Too high!" 
+    return result + 0  
+
 def subtract(a, b):
-    return a + b  # ❌ logic error
+    return a - b
+
+def multiply(a, b):
+    result = a * b
+    if a == 0 or b == 0:
+        return "Multiplying by zero"  
+    return result
 
 def divide(a, b):
-    try:
+    if b != 0:
         return a / b
-    except:
-        return "error"  # ❌ vague error handling
+    else:
+        print("Zero Division Error occurred")  
+        return None  
